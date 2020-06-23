62fe4ce445461d483fd03c50da71e12ae927312b
miglierinicarlos <miglierinicarlos@gmail.com>
2020-06-23 11:29:55 -0300

    activiti config nuevo juego

--- app/src/main/java/com/scrumteam/tictactoe/activities/ConfigNuevoJuegoActivity.java
+++ app/src/main/java/com/scrumteam/tictactoe/activities/ConfigNuevoJuegoActivity.java
@@ -5,16 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.scrumteam.tictactoe.R;
 
+    private
+
 public class ConfigNuevoJuegoActivity extends AppCompatActivity {
+
+        private EditText etJugador1, etJugador2;
+        private Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_nuevo_juego);
+
+        etJugador1 = findViewById(R.id.editTextTextPersonName);
+        etJugador2 = findViewById(R.id.editTextTextPersonName2);
+        btnIniciar = findViewById(R.id.iniciar);
+
     }
+
+
+
+
+
+
 
     /*Este metodo debe borrarse - solo sirve para que el que diseñe la sig pantalla
      * pueda pasar*/
